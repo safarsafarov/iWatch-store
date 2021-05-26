c0a5654ce1a2cd98c59e4f50e1f06ffc1c8f99f3
safarsafarov <safarov.safar@hotmail.com>
2021-05-27 02:54:15 +0800

    commit

---
+++ iWatch/iWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  iWatchApp.swift
+//  iWatch
+//
+//  Created by codestorm on 2021/5/27.
+//
+
+import SwiftUI
+
+@main
+struct iWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
